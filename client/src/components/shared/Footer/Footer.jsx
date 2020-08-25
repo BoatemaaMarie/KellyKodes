2c2737f84919d0847563576037805a2ad2620c65
kellyappiah <kellybappiah@gmail.com>
2020-08-25 19:17:41 -0400

    added footer file

---
+++ client/src/components/shared/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>
+{/* insert github symbol */}
+{/* insert linkedin symbol */}
+      {/* insert email symbol */}
+      <button>Top</button>
+    </div>
+  )
+}
