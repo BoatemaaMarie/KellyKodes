fe3e876737c4133426793271c04d6ed44df973c1
kellyappiah <kellybappiah@gmail.com>
2020-08-26 00:45:57 -0400

    added icons in skills file

--- client/src/screens/Skills/Skills.jsx
+++ client/src/screens/Skills/Skills.jsx
@@ -1,36 +1,47 @@
 import React from 'react'
+import './Skills.css'
 
 export default function Skills() {
   return (
-    <div>
-      <h4>Skills</h4>
-      <p>
-        HTML|
-        CSS|
+    <>
+      <h4 className="skills">SKILLS</h4>
+      <div className="skills-div">
+
+      <div className="skills-container">
+
+      <p className="killset">
+        HTML5|
+        CSS3|
         JavaScript|
         React.JS|  
         Node.JS|  
-        Express.JS|
         Ruby|
         Ruby on Rails|
-        Sequelize|
-        Wordpress|
         SQL|
         Git|
         Github|
-        RestfulAPI|
-        UX/ UI Design|
         MongoDB|
-        Relational Databases|
-        Operations & Logistics|
+        Heroku|
+        jQuery|
         Axios|
-        Visual Design|
         API Development|
-        Web Design|
-        Shopify|
+        Shopify
       </p>
 
-      <img></img>
-    </div>
+      </div>
+      </div>
+      <img src="https://cdn.iconscout.com/icon/free/png-512/html5-10-569380.png"/>
+      <img src="https://cdn.iconscout.com/icon/free/png-512/css-118-569410.png"/>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png"/>
+      <img src="https://icon-library.com/images/node-js-icon/node-js-icon-29.jpg"/>
+      <img src="https://cdn.iconscout.com/icon/free/png-512/ruby-44-1175099.png"/>
+      <img src="https://www.kindpng.com/picc/m/12-120961_logo-ruby-on-rails-hd-png-download.png"/>
+      <img src="https://cdn0.iconfinder.com/data/icons/superuser-extension-light/512/675277-data_database_sql_query-512.png"/>
+      <img src="https://git-scm.com/images/logos/downloads/Git-Icon-Black.png"/>
+      <img src="https://image.flaticon.com/icons/png/512/25/25231.png"/>
+      <img src="https://cdn.iconscout.com/icon/free/png-512/mongodb-5-1175140.png"/>
+      <img src="https://cdn.iconscout.com/icon/free/png-512/heroku-8-1175211.png"/>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRJxgzkRbNpmDsqZbE8zFeZanAmOqUHObnhNQ&usqp=CAU"/>
+    </>
   )
 }
