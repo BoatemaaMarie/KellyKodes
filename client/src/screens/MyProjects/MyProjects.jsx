3d355f9b4faa7bc2688512ae5429b4a0edbae2d4
kellyappiah <kellybappiah@gmail.com>
2020-08-26 16:25:18 -0400

    feature/resume

--- client/src/screens/MyProjects/MyProjects.jsx
+++ client/src/screens/MyProjects/MyProjects.jsx
@@ -20,13 +20,16 @@ export default function MyProjects() {
 
           <div className="all-projects-info">
             <h4>SubBusters</h4>
-
+            <br></br>
+            <div className="line"></div>
+            <br></br>
             <p>SubBusters is an interactive Web Application that allows users to uncover hidden subscriptions they may have forgotten about.
             Users are allowed to view, add and edit subscriptions as well as delete subsciptions.
             I worked with a team of Software Engineers and UX/UI Designers to create a user friendly experience for customers.
             A combination of React and CSS3 was used to generate a multiple-page experience for the user. It uses an API in which we constructed and
             MongoDB for Data Management.
             </p>
+            
             <div className="project-buttons">
 
               <Button variant="outline-light" block>VISIT SITE</Button>
@@ -44,7 +47,10 @@ export default function MyProjects() {
 
           <div className="all-projects-info">
             <h4>Serenity Skins</h4>
-            <p>Serenity Skin Spa is an alrady existing local business, whos mission is to enhance beauty and confidence in their clients by using innovating pratices. The website allows users to login and book appointments. Clients will be able to view treatements provided once they log in. They will also be able to view description, duration and price of the treatement selcted. This interactive site was made using React, CSS3, Ruby on Rails framework for Data Management and PostgreSQL for the database. </p>
+            <br></br>
+            <div className="line"></div>
+            <br></br>
+            <p>Serenity Skin Spa is an already existing local business, who's mission is to enhance beauty and confidence in their clients by using innovating pratices. The website allows users to login and book appointments. Clients will be able to view treatements provided once they log in. They will also be able to view description, duration and price of the treatement selected. This interactive site was made using React, CSS3, Ruby on Rails framework for Data Management and PostgreSQL for the database. </p>
             <div className="project-buttons">
 
               <Button variant="outline-light" block>VISIT SITE</Button>
--- client/src/screens/Resume/Resume.jsx
+++ client/src/screens/Resume/Resume.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import { Button } from 'react-bootstrap'
+import './Resume.css'
 
 export default function Resume() {
   return (
-    <div>
-      <h4>Resume</h4>
-      <p>View my resume and get to know a little about me.</p>
-      <button>View</button>
-    </div>
+    <>
+      <div className="resume-container">
+
+        <div className="resume">
+
+          <h4 className="resume-name">RESUME</h4>
+
+          {/* <div className="line"></div> */}
+          <br></br>
+
+          <p className="resume-para">View my resume and get to know a little about me.</p>
+          
+          <div className="resume-buttons">
+      
+            <Button className="resume-buttons" variant="outline-light" block>VIEW</Button>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
