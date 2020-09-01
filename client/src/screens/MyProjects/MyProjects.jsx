9f417a4122e23364c3a2e2f62ab5732b3605ae02
kellyappiah <kellybappiah@gmail.com>
2020-09-01 01:22:42 -0400

    aesthetic of page

--- client/src/screens/MyProjects/MyProjects.jsx
+++ client/src/screens/MyProjects/MyProjects.jsx
@@ -10,16 +10,16 @@ export default function MyProjects() {
 
 
       <div className="all-projects">
-       
+
         <div className="project-container">
-          
+
           <div className="project-image">
             <img className="all-images" src="http://res.cloudinary.com/dc6ncx5wp/image/upload/c_scale,w_933/v1598466992/Subbusterscreenshot_yqsjow.png" />
             <img className="all-images" src="http://res.cloudinary.com/dc6ncx5wp/image/upload/c_scale,w_885/v1598468222/Screen_Shot_2020-08-26_at_2.55.57_PM_in5wiv.png" />
 
           </div>
 
-          <div className="all-projects-info">
+          <div className="each-project-info">
             <h4>SubBusters</h4>
             <br></br>
             <div className="line"></div>
@@ -30,23 +30,37 @@ export default function MyProjects() {
             A combination of React and CSS3 was used to generate a multiple-page experience for the user. It uses an API in which we constructed and
             MongoDB for Data Management.
             </p>
-            
+
             <div className="project-buttons">
 
-              <Button variant="outline-light" block>VISIT SITE</Button>
+              <a
+                target="_blank"
+                href="http://subbusters.surge.sh/"
+                style={{ textDecoration: 'none' }}>
+                <Button variant="outline-light" block>VISIT SITE</Button>
+              </a>
 
-              <Button variant="outline-light" block>REPO</Button>
+              <a
+                target="_blank"
+                href="https://github.com/thethomasmiller/SubBusters"
+                style={{ textDecoration: 'none' }}>
+                <Button variant="outline-light" block>REPO</Button>
+              </a>
             </div>
 
           </div>
         </div>
-      </div>
 
-
-      <div className="all-projects">
         <div className="project-container">
 
-          <div className="all-projects-info">
+        <div className="project-image">
+            <img className="all-images" src="http://res.cloudinary.com/dc6ncx5wp/image/upload/v1598470258/screenseren_urbxmn.png" />
+
+            <img className="all-images" src="http://res.cloudinary.com/dc6ncx5wp/image/upload/v1598470521/Screen_Shot_2020-08-26_at_3.33.41_PM_db6blf.png" />
+
+          </div>
+
+          <div className="each-project-info">
             <h4>Serenity Skins</h4>
             <br></br>
             <div className="line"></div>
@@ -54,20 +68,24 @@ export default function MyProjects() {
             <p>Serenity Skin Spa is an already existing local business, who's mission is to enhance beauty and confidence in their clients by using innovating pratices. The website allows users to login and book appointments. Clients will be able to view treatements provided once they log in. They will also be able to view description, duration and price of the treatement selected. This interactive site was made using React, CSS3, Ruby on Rails framework for Data Management and PostgreSQL for the database. </p>
             <div className="project-buttons">
 
-              <Button variant="outline-light" block>VISIT SITE</Button>
+              <a
+                target="_blank"
+                href="http://serenityskins.surge.sh"
+                style={{ textDecoration: 'none' }}> <Button variant="outline-light" block>VISIT SITE</Button>
+              </a>
 
-              <Button variant="outline-light" block>REPO</Button>
+              <a
+                target="_blank"
+                href="https://github.com/KellyAppiah/SerenitySkins"
+                style={{ textDecoration: 'none' }}>    <Button variant="outline-light" block>REPO</Button>
+              </a>
             </div>
           </div>
 
-          <div className="project-image">
-            <img className="all-images" src="http://res.cloudinary.com/dc6ncx5wp/image/upload/v1598470258/screenseren_urbxmn.png" />
-
-            <img className="all-images" src="http://res.cloudinary.com/dc6ncx5wp/image/upload/v1598470521/Screen_Shot_2020-08-26_at_3.33.41_PM_db6blf.png" />
-
-          </div>
+         
         </div>
       </div>
+
 
     </>
 
